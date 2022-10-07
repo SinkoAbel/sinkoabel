5db5a2ea5a8894499708ee15855b986bbee3eabb
Sinkó Ábel <sinkoabel@gmail.com>
2022-10-07 21:41:34 +0200

    added slash

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    basePath: "sinkoabel",
-    assetPrefix: "sinkoabel",
+    basePath: "/sinkoabel",
+    assetPrefix: "/sinkoabel",
 }
 
 module.exports = nextConfig
