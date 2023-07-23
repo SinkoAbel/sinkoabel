1f45c97b5b21165dc35a5eac480275a5ab64842c
SinkoAbel <84632718+SinkoAbel@users.noreply.github.com>
2023-07-23 09:32:29 +0000

    Deploying to gh-pages from @ SinkoAbel/sinkoabel@91d775f39c763f418eb0a549dd9d3e39713f3125 🚀

---
+++ _next/static/KunjythMeGox9jKKdx9bW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/721-2f996f45c46644ac.js","static/chunks/pages/index-3192583050d02693.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
