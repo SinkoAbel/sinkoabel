666f71536d710320783b7dcc93c9bcf10ad086db
SinkoAbel <84632718+SinkoAbel@users.noreply.github.com>
2024-06-30 21:56:48 +0000

    Deploying to gh-pages from @ SinkoAbel/sinkoabel@7f2bdc4814d2aa94d02633cf27b998c35aa782e2 🚀

--- _next/static/JLodsoSpPLa0ODKeYt8z9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/721-2f996f45c46644ac.js","static/chunks/pages/index-e6b8947d4a09683a.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
